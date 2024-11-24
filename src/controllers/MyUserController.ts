0d35b32f6a97a57f479ea2c58be829d0ee00b09a
Zümra <zumrakb8@gmail.com>
2024-11-24 22:14:59 +0300

    getuser api, toast, update user, user profile form

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -1,6 +1,22 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
+export const getCurrentUser = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const currentUser = await User.findOne({ _id: req.userId });
+    if (!currentUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(currentUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error getting user" });
+  }
+};
+
 export const createCurrentUser = async (
   req: Request,
   res: Response
--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import {
   createCurrentUser,
+  getCurrentUser,
   updateCurrentUser,
 } from "../controllers/MyUserController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
 import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
+
+router.get("/", jwtCheck, jwtParse, getCurrentUser);
 
 router.post("/", jwtCheck, createCurrentUser); //authanticed backend endpoint.
 
