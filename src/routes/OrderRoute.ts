ca8cf2a6c47a7862d73f28848542862ee51a6d77
Zümra <zumrakb8@gmail.com>
2024-12-07 18:17:22 +0300

    order status backend

--- src/routes/OrderRoute.ts
+++ src/routes/OrderRoute.ts
@@ -2,10 +2,14 @@ import express from "express";
 import { jwtCheck, jwtParse } from "../middleware/auth";
 import {
   createCheckoutSession,
+  getMyOrders,
   stripeWebhookHandler,
 } from "../controllers/OrderController";
 
 const router = express.Router();
+
+router.get("/", jwtCheck, jwtParse, getMyOrders);
+
 router.post(
   "/checkout/create-checkout-session",
   jwtCheck,
